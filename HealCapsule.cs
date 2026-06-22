82c9f5935f0695c55cabfe3ced9d7bec425c01a8
Poterik <skybie50@gmail.com>
2026-06-22 15:44:44 +0300

    Улучшение хилки

--- HealCapsule.cs
+++ HealCapsule.cs
@@ -5,19 +5,21 @@ public class HealCapsule : MonoBehaviour
     private int healPoint = 25;
     private float lifetime = 120f;
 
+    private PlayerFighter fighter;
+
     private void Start()
     {
+        fighter = GameManager.Instance.player;
+
         Destroy(gameObject, lifetime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            PlayerFighter pf = other.GetComponent<PlayerFighter>();
-            int heal = healPoint + GameManager.Instance.recovery;
-            pf.UpdateHealth(heal);
-            Destroy(this.gameObject);
-        }
+        if (!other.CompareTag("Player") || !fighter.CheckHealth()) return;
+
+        int heal = healPoint + GameManager.Instance.recovery;
+        fighter.UpdateHealth(heal);
+        Destroy(this.gameObject);
     }
 }
